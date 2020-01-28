7335e84696fd5d2f49bd496181d886dc4c5a5c8b
unknown <malcomgbugd@gmail.com>
2020-01-28 15:57:01 -0300

    Estoy re locro, falta revisar.

--- ProductoMedianteSumas/ProductoMedianteSumas.cs
+++ ProductoMedianteSumas/ProductoMedianteSumas.cs
@@ -6,46 +6,65 @@ namespace ProductoMedianteSumas
     {
         static void Main(string[] args)
         {
-            int NumerosDispares = 0, valorValidoUno = 0, valorValidoDos = 0;
+            int NumerosDispares = 0, valorValidoUno = 0, valorValidoDos = 0, valorUno = 0, valorDos = 0, resultado = 0;
             while (NumerosDispares != 1)
             {
                 if (valorValidoUno != 1)
                 {
-                    Console.WriteLine("\n Ingrese el primer valor ");
+                    Console.WriteLine("\n ~Ingrese el primer valor~ ");
                     var sValorUno = Console.ReadLine();
                     try
                     {
-                        int valorUno = Int32.Parse(sValorUno);
+                        valorUno = Int32.Parse(sValorUno);
                         valorValidoUno++;
                     }
                     catch (FormatException)
                     {
-                        Console.WriteLine("Ingrese un valor valido");
+                        Console.WriteLine("\n El valor no es valio, pruebe nuevamente");
                     }
                 }
                 if (valorValidoDos != 1)
                 {
-                    Console.WriteLine("\n Ingrese el primer valor ");
+                    Console.WriteLine("\n ~Ingrese el segundo valor ~");
                     var sValorDos = Console.ReadLine();
                     try
                     {
-                        int valorDos = Int32.Parse(sValorDos);
+                        valorDos = Int32.Parse(sValorDos);
                         valorValidoDos++;
                     }
                     catch (FormatException)
                     {
-                        Console.WriteLine("Ingrese un valor valido");
+                        Console.WriteLine("\n El valor no es valio, pruebe nuevamente");
                     }
                 }
                 _ = (valorValidoUno == 1 && valorValidoDos == 1) ? NumerosDispares++ : NumerosDispares = 0;
             }
-            //Tengo los valores validos, ahora calcular suma
+            resultado = Calcular(valorUno, valorDos, resultado);
+            Console.WriteLine("\n El resultado final es: {0}", resultado);
         }
-        int Calcular(int valorUno, int valorDos)
-        {
-            if (numero == 1) 
-            { 
-                // nada
+        static int Calcular(int valorUno, int valorDos, int resultado)
+        {          
+            if (valorDos == 0)
+            {
+                return (resultado);                
+            }
+            else
+            {
+                if (resultado == 0)
+                {
+                    resultado = valorUno + valorUno;
+                    Console.WriteLine($"\n ~ {valorUno} + {valorUno} = {resultado} ");
+                    valorDos--;
+                    Calcular(valorUno, valorDos, resultado);
+                }
+                else 
+                {
+                    Console.WriteLine($"\n ~ {resultado} + {valorUno} = {resultado+valorUno} ");
+                    resultado += valorUno;                    
+                    valorDos--;
+                    Calcular(valorUno, valorDos, resultado);
+                }
+                
             }
             return (resultado);
         }
