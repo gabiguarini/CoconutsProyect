ffdcffa01861d7970bf035ee7b1550821be06f4f
unknown <malcomgbugd@gmail.com>
2020-02-27 12:53:35 -0300

    Server avanzando

---
+++ requesthandlers.js
@@ -0,0 +1,12 @@
+function iniciar() 
+{
+    console.log("Invocar el handler de iniciar")
+}
+
+function subir() 
+{
+    console.log("Invocar el handler de iniciar")    
+}
+
+exports.iniciar = iniciar;
+exports.subir = subir;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var http = require("http");
 var url = require("url");
 
-function iniciar(route) {
+function iniciar(route, handle) {
     function onRequest(request, response)
-    {
+    {        
         var pathname = url.parse(request.url).pathname;
         console.log("Request a "+ pathname +" recibido");
         console.log("Request recibido");
 
-        route(pathname);
-        
+        route(pathname, handle);
+
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write("Holis");
         response.end();
