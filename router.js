d82f9c9a52fcef2ec74dcf3574f138b68198de36
unknown <malcomgbugd@gmail.com>
2020-02-28 11:05:08 -0300

    serv casi funcando

--- router.js
+++ router.js
@@ -1,9 +1,15 @@
-function route(path, handle) {
+function route(path, handle, response, postData) {
     console.log("Ruteando un request a " + path);
     if (typeof handle[path] === 'function')
     {
-        return handle[path]();
-    }else console.log("No hay handler definido para " + path) ;
+        return handle[path](response, postData);
+    }else
+    {
+        console.log("No hay handler definido para " + path);
+        response.writeHead(404, {"Content-Type": "text/html"});
+        response.write("404 No enocntrado");
+        response.end();
+    } 
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -3,17 +3,22 @@ var url = require("url");
 
 function iniciar(route, handle) {
     function onRequest(request, response)
-    {        
+    {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
-        console.log("Request a "+ pathname +" recibido");
-        console.log("Request recibido");
+        console.log("Request a "+ pathname +" recibido"); 
 
-        route(pathname, handle);
-
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("Holis");
-        response.end();
-        console.log("Request terminado");
+        request.setEncoding("utf8");
+        
+        request.addListener("data", function (datos) 
+        {
+            postData += datos;
+            console.log("Post recibido: " + datos + ".");   
+        });
+        request.addListener("End", function () 
+        {
+            route(pathname, handle, response, postData);            
+        });
     }
     http.createServer(onRequest).listen(8888);
     console.log("Serv iniciado");
